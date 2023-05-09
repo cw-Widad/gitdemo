c247ea7ba78617aa4bc3d143232445688a5d0d62
Widad Mogral <widadmogral@gmail.com>
2023-05-09 11:15:32 +0200

    some code

--- lsp.py
+++ lsp.py
@@ -1,4 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/pythoin
+
+#on to some coding
+
+import os
+import argparse
+import stat
+def longformat(filepath):
+    path = os.path.abspath(filepath)
+    filelist = os.listdir(filepath)
+    #[print(os.path.join(path,i)) for i in filelist]
+    for i in filelist:
+        abspath = os.path.join(path,i)
+        stats = os.stat(abspath)
+        if not i.startswith('.'):
+           print(f"{stat.filemode(stats.st_mode)} {stats.st_nlink}      {stats.st_uid}       {stats.st_gid}       {stats.st_size}        {stats.st_atime}      {i}")
 
 
 #comment
